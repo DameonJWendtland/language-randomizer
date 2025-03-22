b706a799d540e16b6ae078f149637455b2904e37
Dameon Jérôme Wendtland <dameon.wendtland@justrelate.com>
2025-03-22 10:37:44 +0100

    App icon

--- App.py
+++ App.py
@@ -4,6 +4,7 @@ from gui import create_main_gui
 def main():
     root = ThemedTk(theme="default")
     root.title("Language Randomizer")
+    root.iconbitmap("C:/Users/micro/PycharmProjects/language-randomizer/translating.ico")
     create_main_gui(root)
     root.mainloop()
 
